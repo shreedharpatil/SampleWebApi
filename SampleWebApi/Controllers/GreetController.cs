e3a3fef0f2188c0d011133006ccded8aa44c7b20
Shreedhar Patil <shreedhar.patil@honeywell.com>
2018-12-03 14:01:37 +0530

    GreetController added

---
+++ SampleWebApi/Controllers/GreetController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebApi.Controllers
+{
+    public class GreetController : Controller
+    {
+        public string Index()
+        {
+            return "Hello! Welcome to azure devops...";
+        }
+    }
+}
